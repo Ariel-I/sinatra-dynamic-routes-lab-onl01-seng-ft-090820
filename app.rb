2d17cec71f017f9a96f42ae821439ad58856b042
Ariel I <arielibarra39@gmail.com>
2020-10-17 04:18:27 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -31,8 +31,12 @@ class App < Sinatra::Base
   end 
   
   get "/:operation/:number1/:number2" do 
-    @operation = params[:number1], params[:number2]
-    ""
+    @operation = params[:operation] 
+    @number1 = params[:number1]
+    @number2 = params[:number2]
+    case @operation
+    
+    
   end 
   
 end
